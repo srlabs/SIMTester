9ee70d81426878816fa7cf35fd9d60d54ff64fac
Gabriel Arnautu <gabriel@srlabs.de>
2020-10-05 15:25:03 +0200

    Add function selectFileByPath

--- SIMLibrary/src/de/srlabs/simlib/FileManagement.java
+++ SIMLibrary/src/de/srlabs/simlib/FileManagement.java
@@ -128,6 +128,88 @@ public class FileManagement {
         return new byte[]{fid_data[2], fid_data[3]};
     }
 
+    /**
+     * This function gets a file path as argument and selects the file using 3G APDU.
+     * It uses P1=08 which means "Select by path from MF"
+     *
+     * @param filePath: the path of the file (without slashes)
+     * @return SimCardFile
+     * @throws CardException
+     * @throws FileNotFoundException
+     */
+    public static SimCardFile selectFileByPathPath(String filePath) throws CardException, FileNotFoundException {
+        // Check if the card accepts 3G APDU
+        if (!SIMLibrary.third_gen_apdu) {
+            return null;
+        }
+
+        CommandAPDU select = new CommandAPDU((byte) 0x00, (byte) 0xA4, (byte) 0x08, (byte) 0x04, HexToolkit.fromString(filePath));
+        ResponseAPDU r = ChannelHandler.transmitOnDefaultChannel(select);
+
+        SimCardFile selectedFile;
+        SelectResponse selectResponse;
+        if ((short) 0x9000 == (short) r.getSW()) {
+            selectResponse = new SelectResponse3G(r.getData());
+        } else if ((short) 0x6A82 == (short) r.getSW()) {
+            throw new FileNotFoundException("file ID: " + filePath + "; doesn't seem to exist on this card; SW = " + Integer.toHexString(r.getSW()));
+        } else {
+            throw new CardException("an unexpected error has occurred during selection of file ID: " + filePath + "; SW = " + Integer.toHexString(r.getSW()) + "; APDU = " + HexToolkit.toString(select.getBytes()));
+        }
+
+        switch (selectResponse.getFileType()) { // fileType byte
+            case SimCardFile.MF:
+                selectedFile = new SimCardMasterFile(selectResponse);
+                if (DEBUG) {
+                    System.out.println(LoggingUtils.formatDebugMessage("selected MF " + selectResponse.getFileId() + ", child DFs: " + selectedFile.getNumberOfChildDFs() + ", child EFs: " + selectedFile.getNumberOfChildEFs()));
+                }
+                break;
+            case SimCardFile.DF:
+            case SimCardFile.ADF:
+                selectedFile = new SimCardDirectoryFile(selectResponse);
+                if (DEBUG) {
+                    System.out.println(LoggingUtils.formatDebugMessage("selected DF " + selectResponse.getFileId() + ", child DFs: " + selectedFile.getNumberOfChildDFs() + ", child EFs: " + selectedFile.getNumberOfChildEFs()));
+                }
+                break;
+
+            case SimCardFile.EF:
+            case SimCardFile.INTERNAL_EF:
+                switch (selectResponse.getEFType()) { // fileStructure byte
+                    case SimCardElementaryFile.EF_TRANSPARENT:
+                        selectedFile = new SimCardTransparentFile(selectResponse);
+                        if (DEBUG) {
+                            System.out.println(LoggingUtils.formatDebugMessage("selected EF Transparent " + selectResponse.getFileId() + ", size: " + selectedFile.getFileSize()));
+                        }
+                        break;
+                    case SimCardElementaryFile.EF_LINEAR_FIXED:
+                        selectedFile = new SimCardLinearFixedFile(selectResponse);
+                        if (DEBUG) {
+                            System.out.println(LoggingUtils.formatDebugMessage("selected EF Linear-Fixed " + selectResponse.getFileId() + ", size: " + selectedFile.getFileSize()));
+                        }
+                        break;
+                    case SimCardElementaryFile.EF_CYCLIC:
+                        selectedFile = new SimCardCyclicFile(selectResponse);
+                        if (DEBUG) {
+                            System.out.println(LoggingUtils.formatDebugMessage("selected EF Cyclic " + selectResponse.getFileId() + ", size: " + selectedFile.getFileSize()));
+                        }
+                        break;
+                    case SimCardElementaryFile.EF_NO_INFO:
+                        selectedFile = new SimCardNoInfoFile(selectResponse);
+                        if (DEBUG) {
+                            System.out.println(LoggingUtils.formatDebugMessage("selected EF NoInfo " + selectResponse.getFileId() + ", size: " + selectedFile.getFileSize()));
+                        }
+                        break;
+                    default:
+                        throw new CardException("Unknown EF type while selecting " + filePath);
+                }
+                break;
+            default:
+                throw new CardException("Unknown file type while selecting " + filePath);
+
+        }
+
+        return selectedFile;
+    }
+
     public static ResponseAPDU selectFileById(byte[] fileId) throws CardException {
         if (DEBUG) {
             System.out.println(LoggingUtils.formatDebugMessage("selecting file: " + HexToolkit.toString(fileId)));
